bf1307d3bad5a2f6d670d4c61fa7644c04f88a68
Łukasz Woźniak <ukasz Woniak>
2017-03-25 15:16:07 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function getTriangleArea(a, h) 
+{
+  if (a > 0 && h > 0) 
+{
+} 
+  else 
+{
+return 'nieprawidłowe dane'
+}
+return a * h/2  
+}
+var triangle1Area = getTriangleArea(4, 6);
+var triangle2Area = getTriangleArea(5, 8);
+var triangle3Area = getTriangleArea(0, 2);
+var triangle4Area = getTriangleArea(2, 0);
+console.log(getTriangleArea(10,6))
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+console.log(triangle4Area);
